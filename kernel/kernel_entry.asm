202d24345f3e515df04bc13ef49093963864c7a2
slep2-0 <195588478+slep2-0@users.noreply.github.com>
2025-07-17 06:14:15 +0300

    Updated comment -- remember to rename here if you rename in kernel.c

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -7,5 +7,6 @@ global _start     ; Export _start symbol for the linker
 _start:
     ; The bootloader already sets the stack pointer (esp).
     ; Here you could clear the .bss section if you had one.
-    call kernel_main ; Call your C kernel function
+	; I choose not to, as there is no need for it.
+    call kernel_main ; Call your C kernel function - for me it's kernel_main, but you may re-implement naming however you like.
     hlt              ; Halt the CPU if kernel_main ever returns
